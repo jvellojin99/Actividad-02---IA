2f81fd42da2d4cedf59d6927501d652b49640ff6
Kevin Camilo Millán Nieto <kmillan@xolit.com>
2023-04-15 15:11:13 -0500

    entrenar el modelo

--- actividad3.py
+++ actividad3.py
@@ -23,4 +23,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # crear el clasificador
 classifier = LogisticRegression(random_state=0)
 
-
+# entrenar el modelo
+classifier.fit(X_train, y_train)

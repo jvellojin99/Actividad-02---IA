0a96cf0d892b49a9fa30a434f33a934892c39d55
Kevin Camilo Millán Nieto <kmillan@xolit.com>
2023-04-15 22:54:26 -0500

    Finalización metodo no supervisado

--- Actividad 04/Actividad04.py
+++ Actividad 04/Actividad04.py
@@ -6,7 +6,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
 # cargar datos
-data = pd.read_csv('C:\\Users\\Joselito\\Documents\\datos2.csv')
+data = pd.read_csv('E:/Work/Disco D/Kevin/Universidad/Inteligencia artificial/Actividades/datos2.csv')
 
 # preprocesamiento de datos
 X = data.iloc[:, 1:].values # seleccionar columnas relevantes
@@ -20,3 +20,19 @@ for i in range(1, 11):
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
 
+plt.plot(range(1, 11), wcss)
+plt.title('Método del codo')
+plt.xlabel('Número de clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# entrenamiento del modelo utilizando el número óptimo de clusters
+kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
+kmeans.fit(X)
+
+# evaluación del modelo utilizando la métrica de silueta
+silhouette_avg = silhouette_score(X, kmeans.labels_)
+print('El coeficiente de silueta promedio es:', silhouette_avg)
+
+# interpretación de resultados
+cluster_labels = kmeans.labels_
